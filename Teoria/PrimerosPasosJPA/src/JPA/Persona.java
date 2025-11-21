01ef7fb1a4dbb44a775fa1e4236e7561408ba126
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2025-11-21 11:01:25 +0100

    Add files via upload

---
+++ Teoria/PrimerosPasosJPA/src/JPA/Persona.java
@@ -0,0 +1,55 @@
+package JPA;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "personas")
+public class Persona {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private int edad;
+    
+    public Persona() {
+        super();
+    }
+    
+    public Persona(String nombre, int edad) {
+        super();
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ Teoria/PrimerosPasosJPA_Parte2/src/com/arquitecturajava/Prueba1JPA.java
@@ -0,0 +1,35 @@
+package com.arquitecturajava;
+
+import com.arquitecturajava.model.Persona;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Prueba1JPA {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadPersonas");
+		EntityManager em = emf.createEntityManager();
+		
+		Persona yo = new Persona("Pedro", 25);
+
+
+		try {
+			em.getTransaction().begin();
+			em.persist(yo);
+			em.getTransaction().commit();
+			System.out.println("Todo ha ido bien");
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			System.err.println("Ha sido un desastre");
+
+		} finally {
+			em.close();
+			emf.close();
+		}
+	}
+}
