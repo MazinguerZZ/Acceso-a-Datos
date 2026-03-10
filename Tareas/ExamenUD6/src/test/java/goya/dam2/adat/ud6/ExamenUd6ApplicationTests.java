cb6af153db137ceceaf09c201a34ed1f0d9ac7b0
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-03-10 12:56:39 +0100

    Add files via upload

---
+++ Tareas/ExamenUD6/src/test/java/goya/dam2/adat/ud6/ExamenUd6ApplicationTests.java
@@ -0,0 +1,13 @@
+package goya.dam2.adat.ud6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenUd6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
